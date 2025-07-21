7148edc2cf3bd130214036daa9bb6166cc1a70fc
Steven J. Selcuk <stevenjselcuk@gmail.com>
2025-07-21 13:51:09 +0300

    dfasdfasdf

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import ReactGA from "react-ga4";
-import "./App.css";
-import App from "./App.jsx";
+import "./app.css";
+import App from "./app.jsx";
 const GA_MEASUREMENT_ID = "G-NRCHSQEMTF";
 ReactGA.initialize(GA_MEASUREMENT_ID);
 createRoot(document.getElementById("root")).render(
