2169f168c96ef6bebd1fc8ab3b41c7bfaa99e52e
Steven J. Selcuk <stevenjselcuk@gmail.com>
2025-09-22 17:13:58 +0300

    social links

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -44,7 +44,7 @@ export default function FooterSection() {
         </div>
         <div className="my-8 flex flex-wrap justify-center gap-6 text-sm">
           <a
-            href="#"
+            href="https://x.com/TabbyCatLLC"
             target="_blank"
             rel="noopener noreferrer"
             aria-label="X/Twitter"
@@ -64,7 +64,7 @@ export default function FooterSection() {
             </svg>
           </a>
           <a
-            href="#"
+            href="https://www.linkedin.com/company/kneeboard-efb/"
             target="_blank"
             rel="noopener noreferrer"
             aria-label="aedIn"
@@ -84,7 +84,7 @@ export default function FooterSection() {
             </svg>
           </a>
           <a
-            href="#"
+            href="https://www.facebook.com/tabbycatllc"
             target="_blank"
             rel="noopener noreferrer"
             aria-label="Facebook"
@@ -129,7 +129,7 @@ export default function FooterSection() {
             </svg>
           </a>
           <a
-            href="#"
+            href="https://www.instagram.com/tabbycatllc"
             target="_blank"
             rel="noopener noreferrer"
             aria-label="Instagram"
@@ -145,26 +145,6 @@ export default function FooterSection() {
               <path
                 fill="currentColor"
                 d="M7.8 2h8.4C19.4 2 22 4.6 22 7.8v8.4a5.8 5.8 0 0 1-5.8 5.8H7.8C4.6 22 2 19.4 2 16.2V7.8A5.8 5.8 0 0 1 7.8 2m-.2 2A3.6 3.6 0 0 0 4 7.6v8.8C4 18.39 5.61 20 7.6 20h8.8a3.6 3.6 0 0 0 3.6-3.6V7.6C20 5.61 18.39 4 16.4 4zm9.65 1.5a1.25 1.25 0 0 1 1.25 1.25A1.25 1.25 0 0 1 17.25 8A1.25 1.25 0 0 1 16 6.75a1.25 1.25 0 0 1 1.25-1.25M12 7a5 5 0 0 1 5 5a5 5 0 0 1-5 5a5 5 0 0 1-5-5a5 5 0 0 1 5-5m0 2a3 3 0 0 0-3 3a3 3 0 0 0 3 3a3 3 0 0 0 3-3a3 3 0 0 0-3-3"
-              ></path>
-            </svg>
-          </a>
-          <a
-            href="#"
-            target="_blank"
-            rel="noopener noreferrer"
-            aria-label="TikTok"
-            className="text-muted-foreground hover:text-primary block"
-          >
-            <svg
-              className="size-6"
-              xmlns="http://www.w3.org/2000/svg"
-              width="1em"
-              height="1em"
-              viewBox="0 0 24 24"
-            >
-              <path
-                fill="currentColor"
-                d="M16.6 5.82s.51.5 0 0A4.28 4.28 0 0 1 15.54 3h-3.09v12.4a2.59 2.59 0 0 1-2.59 2.5c-1.42 0-2.6-1.16-2.6-2.6c0-1.72 1.66-3.01 3.37-2.48V9.66c-3.45-.46-6.47 2.22-6.47 5.64c0 3.33 2.76 5.7 5.69 5.7c3.14 0 5.69-2.55 5.69-5.7V9.01a7.35 7.35 0 0 0 4.3 1.38V7.3s-1.88.09-3.24-1.48"
               ></path>
             </svg>
           </a>
